7f00b547f7a2b2b422d0c89ed63cb3454092296b
Dave W <dviper@gmail.com>
2017-07-20 09:16:39 -0600

    add SearchInput to Header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchInput from "../SearchInput/SearchInput";
 
 const Header = () => {
   return(
     <header className="header-component">
       <h1 className="logo">WHOSPLYNG</h1>
+      <SearchInput />
     </header>
   )
 }
